05fca8263f5cd83232fdb87afb17bc73eaa72d5c
Hassan Habib <hassan.habib@goldenwave.tech>
2024-09-06 17:02:21 +0500

    Add json data for secCertRoadmap certifications

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+// import Roadmap from './component/Roadmap'
+
+export default function App() {
+  return (
+    <div>
+      <h4>Security Certification Roadmap</h4>
+    </div>
+  )
+}
